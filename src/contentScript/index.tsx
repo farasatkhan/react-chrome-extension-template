fd788553077ad5310e6c87c4dd191bec887fc597
Farasat Khan <farasatkahan@gmail.com>
2025-05-27 22:28:47 +0500

    Fixed UI bug caused by tailwind when injecting content to the site

--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "../styles/tailwind.css";
 import ContentScript from "./contentScript";
 
 // Initialize the content script
